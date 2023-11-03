17ac72955a9f4e1860b787f4a0f18c196c3f9709
DiegoPSP <diego.paulo2022@gmail.com>
2023-11-03 03:19:57 -0300

    Concatenação de dados.

--- Tipos de dados.py
+++ Tipos de dados.py
@@ -1,9 +1,10 @@
 codigo = 10
 salario = 1500.00
-nome = 'Diego'
+nome ='Diego'
 situacao = True
 
 tipo = type(salario)
 
 print(salario)
 print(tipo)
+print("O código é:",codigo, "\nO nome é:",nome, "\nO salário atual é:",salario)
