ed87046566eda4785e21c1ad4b655d589ee218b9
DiegoPSP <diego.paulo2022@gmail.com>
2023-12-02 00:31:40 -0300

    Uppdate do código e adição do laço While.

--- Operadores.py
+++ Operadores.py
@@ -5,3 +5,15 @@ C = 20
 print ("A == B AND B > C:", A == B and B > C)
 print("A < B OR B > C:", A < B or B > C)
 print("NOT A == B:", not A == B)
+print ("\n")
+
+x = 15
+while x >= 1:
+    print (x);
+    x = x-1
+
+x = 1
+print ("\n")
+while x <= 15:
+    print(x)
+    x = x+1
